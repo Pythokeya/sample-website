95a037510d0ee1a9afa97b43143a748f6a23e430
Pythokeya <96433280+Pythokeya@users.noreply.github.com>
2024-11-20 14:02:26 +0000

    simply

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello World!</p>"
 
+SERVER_NAME = "Karthikeya"
+
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', 2010, True)

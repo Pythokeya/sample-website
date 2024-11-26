60d5b4393b788e5a2b4495512ef005919cc1bc37
Pythokeya <96433280+Pythokeya@users.noreply.github.com>
2024-11-26 17:39:30 +0000

    almost finished home page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World!</p>"
+    return render_template('home.html')
 
-SERVER_NAME = "Karthikeya"
+SERVER_NAME = "jobec"
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 2010, True)

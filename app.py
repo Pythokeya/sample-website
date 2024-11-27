f075877e95b74837abe07f69881b0a26527e0910
Pythokeya <96433280+Pythokeya@users.noreply.github.com>
2024-11-27 03:27:08 +0000

    added logo and apply, post html pages

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
+
+@app.route("/apply")
+def apply_page():
+    return render_template('apply.html')
+
+@app.route("/post")
+def post_page():
+    return render_template('post.html')
 
 SERVER_NAME = "jobec"
 
